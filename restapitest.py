9d9a60443bfa53feb8a800a607cd14d5290c492c
EUROPE\Aly_Magdy <aly_magdy@dell.com>
2023-08-12 15:11:39 +0300

    Add another function

--- restapitest.py
+++ restapitest.py
@@ -6,6 +6,7 @@ def get_single_user_email(num):
 
 
     response = requests.get(api_url)
+
     if response.status_code == 200:
         joke_data = response.json()
         joke = joke_data['data']
@@ -14,3 +15,29 @@ def get_single_user_email(num):
     else:
         print("Unexpected status code:", response.status_code)
         return None
+
+def add_user(name,job):
+    url = "https://reqres.in/api/users"
+    payload = {
+        "name": name,
+        "job": job
+    }
+
+    response = requests.post(url,json=payload)
+    if response.status_code == 201:
+        user_data = response.json()
+        return user_data
+    else:
+        print("Unexpected status code:", response.status_code)
+        return None
+
+
+    
+def register_user():
+    url = "https://reqres.in/api/register"
+    payload ={
+        "email" : "anymail@gmail.com",
+        "password" : "anyy" 
+    }
+
+    # Complete the code...
--- testcase.robot
+++ testcase.robot
@@ -2,7 +2,16 @@
 Library           restapitest.py
 
 *** Test Cases ***
-Run My Python Script
+Get Single User Data
     Log    Hello
     ${email}=    get_single_user_email    5
     Should Not Be Empty    ${email}
+
+Add Single User
+    ${Data}=    Add User    Anyone    Engineer
+
+    Log To Console    ${Data}
+
+
+Register User
+# Complete adding the Keyword.
